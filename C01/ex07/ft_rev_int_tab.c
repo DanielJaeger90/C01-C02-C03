2f14ff1a5b5f6c137c2ecf4ac5e53aad3d49bed0
Daniel Jaeger <daniel.iturralde.26@gmail.com>
2024-02-21 02:15:17 +0100

    Ejercicios de C01, C02, C03

---
+++ C01/ex07/ft_rev_int_tab.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_rev_int_tab.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: marvin <marvin@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/02/21 02:09:17 by marvin            #+#    #+#             */
+/*   Updated: 2024/02/21 02:14:04 by marvin           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	ft_rev_int_tab(int *tab, int size)
+{
+	int	i;
+	int	element;
+
+	i = 0;
+	while (i < size / 2)
+	{
+		element = tab[i];
+		tab[i] = tab[size - i - 1];
+		tab[size - i - 1] = element;
+		i++;
+	}
+}
